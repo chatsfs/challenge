86462b0f6e7f9e927a13d7661d419c48515c2aa7
rfb <rfbigoria@gmail.com>
2022-03-14 13:27:00 -0500

    feat(game-02-tests): Tests setted

--- game-02/test/gilded-rose.spec.ts
+++ game-02/test/gilded-rose.spec.ts
@@ -1,6 +1,6 @@
 const assert = require("assert");
 const { expect } = require("chai");
-const { GildedRose, Item } = require("../app/gilded-rose");
+const { GildedRose, Item, Names } = require("../app/gilded-rose");
 
 function assertItemEq(actual, expected) {
   expect(actual.name).to.be.eq(expected.name);
@@ -10,52 +10,66 @@ function assertItemEq(actual, expected) {
 
 describe("Sulfuras tests", () => {
   it("Sulfuras inmutable", () => {
-    const result = GildedRose.updateQuality();
-    // assert.equal(result, 5);
-    expect(result).to.be.eq(4);
-  });
-  it("Sulfuras inmutable", () => {
-    const result = GildedRose.updateQuality();
-    // assert.equal(result, 5);
-    expect(result).to.be.eq(4);
+    const gildedRose = new GildedRose([new Item(Names.SULFURAS, 20, 80)]);
+    gildedRose.updateQuality();
+    assertItemEq(gildedRose.items[0], new Item(Names.SULFURAS, 20, 80));
   });
 });
 
-describe("Aged brie tests", () => {
-  it("Sulfuras inmutable", () => {
-    const result = GildedRose.updateQuality();
-    // assert.equal(result, 5);
-    expect(result).to.be.eq(4);
+describe("Brie tests", () => {
+  it("Increase quality by getting older", () => {
+    const gildedRose = new GildedRose([new Item(Names.BRIE, 20, 40)]);
+    gildedRose.updateQuality();
+    assertItemEq(gildedRose.items[0], new Item(Names.BRIE, 19, 41));
   });
-  it("Sulfuras inmutable", () => {
-    const result = GildedRose.updateQuality();
-    // assert.equal(result, 5);
-    expect(result).to.be.eq(4);
+
+  it("Increase quality faster when expirated", () => {
+    const gildedRose = new GildedRose([new Item(Names.BRIE, -1, 32)]);
+    gildedRose.updateQuality();
+    assertItemEq(gildedRose.items[0], new Item(Names.BRIE, -2, 34));
   });
 });
 
-describe("Backstage passes tests", () => {
-  it("Sulfuras inmutable", () => {
-    const result = GildedRose.updateQuality();
-    // assert.equal(result, 5);
-    expect(result).to.be.eq(4);
+describe("Backstage tests", () => {
+  it("Increase quality by getting near the date", () => {
+    const gildedRose = new GildedRose([new Item(Names.BACKSTAGE_PASS, 20, 40)]);
+    gildedRose.updateQuality();
+    assertItemEq(gildedRose.items[0], new Item(Names.BACKSTAGE_PASS, 19, 41));
   });
-  it("Sulfuras inmutable", () => {
-    const result = GildedRose.updateQuality();
-    // assert.equal(result, 5);
-    expect(result).to.be.eq(4);
+
+  it("Increase quality faster 10 days or less", () => {
+    const gildedRose = new GildedRose([new Item(Names.BACKSTAGE_PASS, 9, 32)]);
+    gildedRose.updateQuality();
+    assertItemEq(gildedRose.items[0], new Item(Names.BACKSTAGE_PASS, 8, 34));
+  });
+  it("Increase quality faster 5 days or less", () => {
+    const gildedRose = new GildedRose([new Item(Names.BACKSTAGE_PASS, 4, 32)]);
+    gildedRose.updateQuality();
+    assertItemEq(gildedRose.items[0], new Item(Names.BACKSTAGE_PASS, 3, 35));
+  });
+  it("Quality drop to 0", () => {
+    const gildedRose = new GildedRose([new Item(Names.BACKSTAGE_PASS, 0, 32)]);
+    gildedRose.updateQuality();
+    assertItemEq(gildedRose.items[0], new Item(Names.BACKSTAGE_PASS, -1, 0));
+  });
+
+  it("Quality remains in 0", () => {
+    const gildedRose = new GildedRose([new Item(Names.BACKSTAGE_PASS, -1, 0)]);
+    gildedRose.updateQuality();
+    assertItemEq(gildedRose.items[0], new Item(Names.BACKSTAGE_PASS, -2, 0));
   });
 });
 
-describe("Conjured items tests", () => {
-  it("Sulfuras inmutable", () => {
-    const result = GildedRose.updateQuality();
-    // assert.equal(result, 5);
-    expect(result).to.be.eq(4);
+describe("Conjured tests", () => {
+  it("Decrease quality by getting older", () => {
+    const gildedRose = new GildedRose([new Item(Names.CONJURED, 20, 40)]);
+    gildedRose.updateQuality();
+    assertItemEq(gildedRose.items[0], new Item(Names.CONJURED, 19, 38));
   });
-  it("Sulfuras inmutable", () => {
-    const result = GildedRose.updateQuality();
-    // assert.equal(result, 5);
-    expect(result).to.be.eq(4);
+
+  it("Decrease quality faster when expirated", () => {
+    const gildedRose = new GildedRose([new Item(Names.CONJURED, -1, 32)]);
+    gildedRose.updateQuality();
+    assertItemEq(gildedRose.items[0], new Item(Names.CONJURED, -2, 28));
   });
 });
